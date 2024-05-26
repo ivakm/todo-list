4d2bcab6de0311e3805aae9656b135cacdbacb88
Shevchenko Dmytro <dimashevchenko594@gmail.com>
2024-05-26 23:07:09 +0300

    created router for the todo path

---
+++ routes/todoRoute.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const router = express.Router();
+const todoController = require('../controllers/todoController');
+
+router.get('/', todoController.getAllTodos);
+router.get('/:id', todoController.getTodoById);
+router.post('/', todoController.createTodo);
+router.put('/:id', todoController.updateTodoById);
+router.delete('/:id', todoController.removeTodoById);
+
+module.exports = router;
