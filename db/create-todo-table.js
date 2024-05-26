b3b22b60f8a18e3fd7ba8a667b20d44815ef74fc
Shevchenko Dmytro <dimashevchenko594@gmail.com>
2024-05-26 20:11:56 +0300

    created db connection, installed .dotenv

---
+++ db/create-todo-table.js
@@ -0,0 +1,12 @@
+﻿// TODO I left it as an example so it should be removed in the future
+
+const CreateTodoTable = `
+    CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL
+        text VARCHAR(255) NOT NULL
+        creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+`;
+
+module.exports = CreateTodoTable;
