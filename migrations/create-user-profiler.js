efda6b5dd266c1b5751af95db96a0ecddb179a66
Shevchenko Dmytro <dimashevchenko594@gmail.com>
2024-05-30 00:16:27 +0300

    created user model

--- migrations/create-user-profiler.js
+++ migrations/create-user-profiler.js
@@ -1,16 +1,15 @@
 ﻿"use strict";
 
 const profiler = `
-    CREATE TABLE Profiles
+    CREATE TABLE profiles
     (
         profile_id SERIAL PRIMARY KEY,
         user_id    INTEGER             NOT NULL,
         name       VARCHAR(255)        NOT NULL,
         surname    VARCHAR(255)        NOT NULL,
         email      VARCHAR(255) UNIQUE NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES Users (user_id)
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
     );
 `;
-
 
 module.exports = profiler;
