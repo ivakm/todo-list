69b14eba5b079ea9110d722f9514ed1a0b55f409
Shevchenko Dmytro <dimashevchenko594@gmail.com>
2024-06-05 16:01:03 +0300

    created signUp controller

---
+++ controllers/signupController.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+const signUp = async (req, res) => {
+    try {
+        res.status(200).json({ message: "Bearer 123456" });
+    } catch (error) {
+        res.status(500).json({
+            message: "Authentication error",
+            error: error.message,
+        });
+    }
+};
+
+module.exports = {
+    signUp,
+};
--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -2,9 +2,8 @@
 
 const express = require("express");
 const router = express.Router();
+const signUp = require("../controllers/signupController");
 
-router.post("/", async (req, res) => {
-    res.status(200).json({ message: "created" });
-});
+router.post("/", signUp.signUp);
 
 module.exports = router;
