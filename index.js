05cdde770afa371f5b530fb257f7073602bf8a9f
Shevchenko Dmytro <dimashevchenko594@gmail.com>
2024-06-05 15:48:21 +0300

    created routing for the signing up

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const express = require("express");
 const app = express();
 const todoRoutes = require("./routes/todoRoute");
+const signupRoutes = require("./routes/signupRoutes");
 
 require("dotenv").config();
 app.use(express.json());
 
 app.use("/todo", todoRoutes);
+app.use("/signup", signupRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`);
---
+++ routes/signupRoutes.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    res.status(200).json({ message: "created" });
+});
+
+module.exports = router;
